594a27e26a0d449d3a2fbaf1a9406e2d9b745305
dsbowen <dsbowen@wharton.upenn.edu>
2022-03-08 00:08:25 +0000

    Updated instructions for tutorials 0-5.

--- tests/test_src.py
+++ tests/test_src.py
@@ -1,9 +1,13 @@
+import os
+
 import pytest
 from hemlock import User, create_test_app
 from hemlock.app import db
 
 import src
 
+DATA_DIR = "data"
+DATA_FILE = "test.csv"
 N_USERS = 100
 
 
@@ -17,3 +21,6 @@ def app():
 
 def test(app):
     User.test_multiple_users(N_USERS)
+    if not os.path.exists(DATA_DIR):
+        os.mkdir(DATA_DIR)
+    User.get_all_data().to_csv(os.path.join(DATA_DIR, DATA_FILE), index=False)
